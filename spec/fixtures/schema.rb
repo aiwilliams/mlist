75827e1a1f67350078c145d1999d872c97134898
Adam Williams <adam@thewilliams.ws>
2008-12-18 16:56:40 -0500

    Added some indexes

--- spec/fixtures/schema.rb
+++ spec/fixtures/schema.rb
@@ -4,22 +4,25 @@ ActiveRecord::Schema.define(:version => 20081126181722) do
   create_table :mails, :force => true do |t|
     t.column :mail_list_id, :integer
     t.column :thread_id, :integer
-    # TODO Verify length limit on Message-Id header
     t.column :identifier, :string
-    # TODO Verify length limit on Subject header
     t.column :subject, :string
     t.column :email_text, :text
     t.column :created_at, :datetime
   end
+  add_index :mails, :mail_list_id
+  add_index :mails, :thread_id
+  add_index :mails, :identifier
+  add_index :mails, :subject
   
   create_table :threads, :force => true do |t|
     t.column :mail_list_id, :integer
   end
+  add_index :threads, :mail_list_id
   
   create_table :mail_lists, :force => true do |t|
-    # TODO Verify length limit on List-Id header
     t.column :identifier, :string
   end
+  add_index :mail_lists, :identifier
   
   # Database list manager tables
   create_table :lists, :force => true do |t|
