eb9501e57a7dd8cad290049abbe690d75c6d0e69
Adam Williams <adam@thewilliams.ws>
2009-03-03 15:07:08 -0500

    Improved use of string in test

--- spec/models/util/header_sanitizer_spec.rb
+++ spec/models/util/header_sanitizer_spec.rb
@@ -7,13 +7,13 @@ describe MList::Util::HeaderSanitizerHash do
   
   %w(to cc bcc from reply-to).each do |header|
     it %Q{should escape " and \\ in address phrase for #{header}} do
-      @sanitizer[header].call('UTF-8', %{"Johnny " Dangerously \\" <johnny@nomail.net>}).should == [%{"Johnny \\" Dangerously \\\\" <johnny@nomail.net>}]
+      @sanitizer[header].call('UTF-8', '"Johnny " Dangerously \" <johnny@nomail.net>').should == ['"Johnny \" Dangerously \\\\" <johnny@nomail.net>']
     end
   end
   
   %w(sender errors-to).each do |header|
     it %Q{should escape " in address phrase for #{header}} do
-      @sanitizer[header].call('UTF-8', %{"Johnny " Dangerously \\" <johnny@nomail.net>}).should == %{"Johnny \\" Dangerously \\\\" <johnny@nomail.net>}
+      @sanitizer[header].call('UTF-8', '"Johnny " Dangerously \" <johnny@nomail.net>').should == '"Johnny \" Dangerously \\\\" <johnny@nomail.net>'
     end
   end
 end
